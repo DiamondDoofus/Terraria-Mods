ecf7d22224c28b4ae0dd22da839e128853b2279a
DiamondDoofus <157350132+DiamondDoofus@users.noreply.github.com>
2024-08-14 11:17:20 -0400

    Add files via upload

--- Retaliation/DamageClassData/PlayerDamageTracker.cs
+++ Retaliation/DamageClassData/PlayerDamageTracker.cs
@@ -20,7 +20,7 @@ namespace Retaliation.DamageClassData
         {
             base.OnHurt(info);
 
-            // Save damage, potential damage, and attackers NPC ID if available
+            // Save damage, potential damage, and attackers NPC ID if available. If no NPC is found, SourceNPCIndex returns -1.
             int[] quickDamageData = [info.Damage, info.SourceDamage, info.DamageSource.SourceNPCIndex];
             playerDamagerTracker.AddDamageStats(quickDamageData);
         }
